a4fb3ad7241ac00f1a8c14e13a81fa91e87ef974
Pierantozzi  Jacopo <j.pierantozzi@suntec.local>
2025-05-08 12:37:34 +0200

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "react-app",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  rules: {
+    // personalizza qui se vuoi
+  },
+};
